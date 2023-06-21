658df8b8e8ddfea44777fca916acf0175eb2cbee
Sergio Burlamaqui <sergio_o.b_junior@hotmail.com>
2023-06-21 02:33:50 -0300

    Correção identação e aplicação do MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import { useState } from 'react'
 import { login } from './login'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 
 function App() {
   return (
--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,16 +1,35 @@
+import { Button, CssBaseline, Container, Grid } from "@mui/material";
+
 export function login() {
   return (
     <div>
-      <h2>Faça seu login</h2>
-      <label>
-        Email <input name="myEmail"/>
-      </label>
-      <label>
-        Senha <input name="myPassword"/>
-      </label>
-      <a href="/*">Esqueci minha senha</a>
-      <button>Entrar</button>
-      <a href="/*">Ainda não tenho uma conta</a>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <h2>Faça seu login</h2>
+          </Grid>
+          <Grid item>
+            <label>
+              Email <input name="myEmail" />
+            </label>
+          </Grid>
+          <Grid item>
+            <label>
+              Senha <input name="myPassword" />
+            </label>
+          </Grid>
+          <Grid item>
+            <a href="/*">Esqueci minha senha</a>
+          </Grid>
+          <Grid item>
+            <Button variant="contained">Entrar</Button>
+          </Grid>
+          <Grid item>
+            <a href="/*">Ainda não tenho uma conta</a>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
