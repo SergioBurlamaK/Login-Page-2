6fa505221edac9817985560829ee9ebb7a243222
Sergio Burlamaqui <sergio_o.b_junior@hotmail.com>
2023-06-22 13:10:57 -0300

    Background Aplication

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,31 +1,44 @@
-import { Button, CssBaseline, Container, Grid, TextField } from "@mui/material";
+import { Button, CssBaseline, Container, Grid, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import "./style.css";
+import backgroundImage from '../assets/backgroung.jpg';
+
 
 export function login() {
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
+
   return (
-    <div>
+    <Box className="app" component="div" style={backgroundStyle}>
       <CssBaseline />
       <Container maxWidth="sm">
-        <Grid container direction="column" spacing={2}>
+        <Grid container direction="column" spacing={2} justifyContent="flex-start" alignItems="flex-start">
           <Grid item>
-            <h2>Faça seu login</h2>
+            <Typography variant="h4">Faça seu login</Typography>
           </Grid>
           <Grid item>
-            <TextField  id="outline-required" label="Email" size="small"/>
+            <TextField id="outline-required" label="Email" size="small" />
           </Grid>
           <Grid item>
-          <TextField  id="outline-required" label="Password" size="small" type="password"/>
+            <TextField id="outline-required" label="Password" size="small" type="password" />
           </Grid>
           <Grid item>
-            <a href="/*">Esqueci minha senha</a>
-          </Grid>
-          <Grid item>
-            <Button variant="contained">Entrar</Button>
-          </Grid>
-          <Grid item>
-            <a href="/*">Ainda não tenho uma conta</a>
+            <Grid item>
+                <a href="/*" className="smallFont">Esqueci minha senha</a>
+              <Grid item>
+                <Button variant="contained">Entrar</Button>
+              </Grid>
+              <Grid item>
+                  <a href="/*" className="smallFont">Ainda não tenho uma conta</a>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
-    </div>
+    </Box>
   );
 }
+
